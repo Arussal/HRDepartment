88971985b7fbee1631fa9ef872d4391a9e870c1a
Arussal <arussal@mail.ru>
2015-08-21 18:16:14 +0300

    test

--- src/main/com/mentat/nine/Main.java
+++ src/main/com/mentat/nine/Main.java
@@ -7,9 +7,13 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import main.com.mentat.nine.dao.CandidateDAO;
 import main.com.mentat.nine.dao.exceptions.PersistException;
 import main.com.mentat.nine.domain.ApplicationForm;
+import main.com.mentat.nine.domain.CVForm;
+import main.com.mentat.nine.domain.Candidate;
 import main.com.mentat.nine.domain.HRDepartment;
+import main.com.mentat.nine.domain.exceptions.NoSuitableCandidateException;
 
 /**
  * @author Ruslan
@@ -19,8 +23,10 @@ public class Main {
 
 	/**
 	 * @param args
+	 * @throws PersistException 
+	 * @throws NoSuitableCandidateException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuitableCandidateException, PersistException {
 		HRDepartment hr = null;
 		try {
 			hr = new HRDepartment();
@@ -36,13 +42,31 @@ public class Main {
 		int workExpirience = 5;
 		Date date = new Date();
 		ApplicationForm af = hr.formApplicationForm(age, education, requirements, post, salary, workExpirience, date);
-		try {
-			af = hr.createApplicationForm(af);
-		} catch (PersistException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		af = hr.createApplicationForm(af);
 
+		CandidateDAO dao = new CandidateDAO();
+		Set<Candidate> cands = dao.getAllCandidates();
+		System.out.println(cands.size());
+	//	for (Candidate cand : cands) {
+	//		dao.deleteCandidate(cand);
+	//
+		}
+		
+		Candidate candidate = new Candidate();
+		String name = "name";
+		Set<String> skills = requirements;
+		String phone = "phone";
+		String email = "email";
+		int desiredSalary = salary;
+		String additionalInfo = "";
+		CVForm cv = candidate.formCVForm(name, age, skills, education, phone, email, desiredSalary, additionalInfo, post, workExpirience);
+		candidate.sendCVForm(cv, hr);
+
+		
+		Set<Candidate> candidates = hr.findCandidates(af);
+		for(Candidate cand : candidates){
+			System.out.println(cand);
+		}
 
 	}
 
