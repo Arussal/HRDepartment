159100af6acd7adaa3cc7a0fb843844a4c57ed98
arussal <arussal@mail.ru>
2015-08-28 01:23:41 +0300

    create first servlet

---
+++ src/main/com/mentat/nine/ui/HomePageServlet.java
@@ -0,0 +1,44 @@
+package main.com.mentat.nine.ui;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/")
+public class HomePageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomePageServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		performTask(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		performTask(request, response);
+	}
+
+	private void performTask(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+
+		request.getRequestDispatcher("/WEB-INF/pages/main.jsp").forward(request, response);
+	}
+}
