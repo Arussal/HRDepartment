8a863c5e34b9c0d952cee62015d9cf5d3a7eec47
Arussal <arussal@mail.ru>
2015-09-08 17:29:10 +0300

    complete domain model

---
+++ src/main/com/mentat/nine/domain/Applicant.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package main.com.mentat.nine.domain;
+
+/**
+ * @author Ruslan
+ *
+ */
+public class Applicant {
+
+	private Integer id;
+	private String login;
+	private String password;
+
+	public Applicant() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ src/main/com/mentat/nine/domain/Manager.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package main.com.mentat.nine.domain;
+
+/**
+ * @author Ruslan
+ *
+ */
+public class Manager {
+
+	private Integer id;
+	private String login;
+	private String password;
+	
+	public Manager() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Manager other = (Manager) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		return true;
+	}	
+}
